dc04fb4b7a2830919b08b285579dabd036bf14de
nagarjuna950 <49774484+nagarjuna950@users.noreply.github.com>
2021-01-09 05:01:55 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', {
-    title: 'Welcome to Cavinkare  !',   
+    title: 'Welcome to Contoso!',   
   });
 });
 
